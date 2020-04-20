29bf5bfe4e50c472f93e1108ce5545d71cc2e90c
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-04-20 20:49:56 -0300

    add code to pass all teste

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, cur): number => {
+      if (cur.type === 'income') {
+        return acc + cur.value;
+      }
+      return acc;
+    }, 0);
+    const outcome = this.transactions.reduce((acc, cur): number => {
+      if (cur.type === 'outcome') {
+        return acc + cur.value;
+      }
+      return acc;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const newTransaction = new Transaction({ title, type, value });
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
